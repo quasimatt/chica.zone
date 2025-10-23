49c18f6e9072a192126e0d5871dfd73308671b55
quasimatt <50529262+quasimatt@users.noreply.github.com>
2025-10-23 17:08:53 +0200

    Update title of comic 23 to 'A Quiet Reflection'

--- comics.data.js
+++ comics.data.js
@@ -22,7 +22,7 @@ const COMICS = {
   20: { image: '/comics/changa20.png', date: '2025-10-20', title: "Cass's Big Question" },
   21: { image: '/comics/changa21.png', date: '2025-10-21', title: "David's Diagnosis" },
   22: { image: '/comics/changa22.png', date: '2025-10-22', title: "Maisy's Medication Malfunction" },
-  23: { image: '/comics/changa23.png', date: '2025-10-23', title: "The Mob's Mending" }
+  23: { image: '/comics/changa23.png', date: '2025-10-23', title: "A Quiet Reflection" }
 };
 
 // Expose to browser and Node

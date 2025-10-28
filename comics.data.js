4972e7db84058ec354dbccf32027fe252f715875
quasimatt <50529262+quasimatt@users.noreply.github.com>
2025-10-28 17:28:07 +0100

    Update title of comic 28 to 'Baka Moment!'

--- comics.data.js
+++ comics.data.js
@@ -27,7 +27,7 @@ const COMICS = {
   25: { image: '/comics/changa25.png', date: '2025-10-25', title: "Chicos Nuevos" },
   26: { image: '/comics/changa26.png', date: '2025-10-26', title: "Lee's Lady" },
   27: { image: '/comics/changa27.png', date: '2025-10-27', title: "David's Fruity Advice" },
-  28: { image: '/comics/changa28.png', date: '2025-10-28', title: "Kathy's Coffee Order" }
+  28: { image: '/comics/changa28.png', date: '2025-10-28', title: "Baka Moment!" }
 };
 
 // Expose to browser and Node
